55436de63220b231eb24cf9e7c419d7a7904c82c
implodingduck <zsek320@gmail.com>
2021-12-07 15:37:19 -0600

    adding back in the firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,16 +137,16 @@ resource "azurerm_storage_account" "sa" {
   tags = local.tags
 }
 
-# resource "azurerm_storage_account_network_rules" "fw" {
-#   depends_on = [
-#     azurerm_app_service_virtual_network_swift_connection.example
-#   ]
-#   storage_account_id = azurerm_storage_account.sa.id
+resource "azurerm_storage_account_network_rules" "fw" {
+  depends_on = [
+    azurerm_app_service_virtual_network_swift_connection.example
+  ]
+  storage_account_id = azurerm_storage_account.sa.id
 
-#   default_action             = "Deny"
+  default_action             = "Deny"
 
-#   virtual_network_subnet_ids = [azurerm_subnet.functions.id]
-# }
+  virtual_network_subnet_ids = [azurerm_subnet.functions.id]
+}
 
 resource "azurerm_app_service_plan" "asp" {
   name                = "asp-${local.func_name}"
