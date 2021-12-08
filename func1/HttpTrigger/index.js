fb3b21e3ae742972f375b7583d982354c944dfb1
implodingduck <zsek320@gmail.com>
2021-12-08 10:53:14 -0600

    working on calling the private function

--- func1/HttpTrigger/index.js
+++ func1/HttpTrigger/index.js
@@ -1,13 +1,14 @@
+const axios = require('axios');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    context.log("preaxios")
+    const res = await axios.get('https://funcvnetntb4g10spriv.azurewebsites.net/api/httptrigger');
+    context.log(res.data)
+    context.log("postaxios")
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: res.data
     };
 }
